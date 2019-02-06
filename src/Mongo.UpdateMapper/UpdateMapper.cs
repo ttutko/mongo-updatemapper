a2ce105484f803ec897542fce7bb40b02d785775
Tom <ttutko@gmail.com>
2019-02-05 21:38:53 -0500

    Simple case of updating all properties working.

--- src/Mongo.UpdateMapper/UpdateMapper.cs
+++ src/Mongo.UpdateMapper/UpdateMapper.cs
@@ -1,4 +1,7 @@
 using MongoDB.Driver;
+using System.Reflection;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Mongo.UpdateMapper
 {
@@ -6,7 +9,19 @@ namespace Mongo.UpdateMapper
     {
         public static MongoDB.Driver.UpdateDefinition<TDest> CreateUpdate<TDest,TSource>(TSource update)
         {
-            return Builders<TDest>.Update.Set("property", "value");
+            List<UpdateDefinition<TDest>> updateDefinitions = new List<UpdateDefinition<TDest>>();
+            var destProperties = typeof(TDest).GetProperties().ToList();
+            foreach(var srcProp in typeof(TSource).GetProperties())
+            {
+                var matchingProperty = destProperties.Where(p => p.Name == srcProp.Name).SingleOrDefault();
+                if(matchingProperty != null)
+                {
+                    var curUpdate = Builders<TDest>.Update.Set(matchingProperty.Name, srcProp.GetValue(update).ToString());
+                    updateDefinitions.Add(curUpdate);
+                }
+            }
+
+            return Builders<TDest>.Update.Combine(updateDefinitions);
         }
     }
 }
--- test/Mongo.UpdateMapper.Test/UpdateMapperTests.cs
+++ test/Mongo.UpdateMapper.Test/UpdateMapperTests.cs
@@ -1,4 +1,8 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using Xunit;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Mongo.UpdateMapper.Test
 {
@@ -12,6 +16,28 @@ namespace Mongo.UpdateMapper.Test
             var updateFilter = UpdateMapper.CreateUpdate<MyDocument, MyDocumentUpdate>(update);
 
             Assert.IsAssignableFrom(typeof(MongoDB.Driver.UpdateDefinition<MyDocument>), updateFilter);
+        }
+
+        [Fact]
+        public void CreateUpdateReturnsUpdateForAllFields()
+        {
+            MyDocumentUpdate update = new MyDocumentUpdate() { Field1 = "updated", Field2 = "updated", Field3 = "updated"};
+
+            var updateFilter = UpdateMapper.CreateUpdate<MyDocument, MyDocumentUpdate>(update);
+
+            var bson = updateFilter.Render(BsonSerializer.LookupSerializer<MyDocument>(), BsonSerializer.SerializerRegistry);
+            var json = JObject.Parse(bson.ToJson());
+            
+            var expected = new JObject() {
+                {"$set", new JObject {
+                        {"Field1", "updated"},
+                        {"Field2", "updated"},
+                        {"Field3", "updated"}
+                    }
+                }
+            };
+
+            Assert.True(JToken.DeepEquals(json, expected));
         }
     }
 }
